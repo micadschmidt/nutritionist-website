dc724c6bf81b2311eb406f986fb876fac351dafc
micaela dauria <micaelaschmidt47@gmail.com>
2024-05-27 10:13:09 -0300

    About me changes

--- src/pages/AboutMe/index.jsx
+++ src/pages/AboutMe/index.jsx
@@ -16,10 +16,11 @@ const AboutMe = () => {
         </div>
 
         <div className='textContainer'>
-          <h1 className='title-two'>Hi, I'm Cami Rosenbrock, MS</h1>
-          <p className='bio-item'>I am an Integrative & Functional Clinical Nutritionist and Health Coach. My mission is to help you heal your body through a holistic, science-based, root cause approach which takes into consideration your lifestyle factors such as food, sleep, stress, family, fitness, and spirituality.</p>
-          <p className='bio-item'>As a certified yoga instructor and loving mom of four, I believe that peace can be found around food & body when we lead the journey with self-compassion. Your body has wisdom and is yearning for you to tune in to what it’s saying, and I am here to help!</p>
-          <h2 className='title-two about'>My education and training include:</h2>
+          <h1 className='title-two'>¡Hola! Soy Camila Rosenbrock</h1>
+          <p className='bio-item'>Soy nutricionista certificada por la Universidad Adventista del Plata, Argentina,  y especialista avalada por el Instituto Deporte y Vida.
+          Soy una apasionada por ayudar a las personas a alcanzar sus metas de salud y bienestar a través de la <strong>alimentación adecuada y un estilo de vida saludable.</strong></p>
+          <p className='bio-item'> Con 6 de experiencia en el campo de la nutrición, he tenido el privilegio de trabajar con personas de todas las edades y diversos trasfondos para mejorar su calidad de vida a través de la nutrición.</p>
+          {/* <h2 className='title-two about'>My education and training include:</h2>
           <ul className='indented'>
             <li>
               <p className='bio-item'>
@@ -41,16 +42,16 @@ const AboutMe = () => {
                 SERV Safe Certification
               </p>
             </li>
-          </ul>
-          <h3 className='title-two about'>What is the Functional Nutritional approach?</h3>
+          </ul> */}
+          <h3 className='title-two about'>Algo de tu formacion / ideologia que te haga única en tu area</h3>
           <p className='bio-item'>
-            Functional nutrition determines how and why our body’s imbalances occur and restores health by addressing the root causes of these imbalances for each individual.
+            <del>Functional nutrition determines how and why our body’s imbalances occur and restores health by addressing the root causes of these imbalances for each individual.</del>
           </p>
           <p className='bio-item'>
-            The functional medicine model for nutrition is an individualized, client-centered, science-based framework which considers genetic, biochemical, and lifestyle factors which will guide our work together to help achieve your optimal health.
+            <del>The functional medicine model for nutrition is an individualized, client-centered, science-based framework which considers genetic, biochemical, and lifestyle factors which will guide our work together to help achieve your optimal health.</del>
           </p>
           <p className='bio-item'>
-            By addressing root cause, rather than symptoms, we can get a better understanding of the complexity of client conditions. One condition may have many different causes and, one cause many different conditions. Everyone’s path to wellness is unique and I look forward to helping you along it.
+            <del>By addressing root cause, rather than symptoms, we can get a better understanding of the complexity of client conditions. One condition may have many different causes and, one cause many different conditions. Everyone’s path to wellness is unique and I look forward to helping you along it.</del>
           </p>
         </div>
       </div>
