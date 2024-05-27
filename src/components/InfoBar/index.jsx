a40f1661e8ed3ed8364ff9ba380f53c3d863a9e2
micaela dauria <micaelaschmidt47@gmail.com>
2024-05-27 10:13:00 -0300

    InfoBar changes

--- src/components/InfoBar/index.jsx
+++ src/components/InfoBar/index.jsx
@@ -17,8 +17,9 @@ const InfoBar = () => {
 
         <article className='textContainer-two'>
           <h2 className='title-two'>Descripcion corta del servicio principial/Ideologia</h2>
-          <p className='value-prop-summary'>Functional nutrition is a personalized, science-based approach that focuses on identifying and treating the root causes of health imbalances, rather than just symptoms. It considers each individual's unique genetic, biochemical, and lifestyle factors, including diet, to create a tailored health plan. It acknowledges that a single condition can have multiple causes, and one cause can lead to various conditions, depending on an individual's specific circumstances. This method provides a comprehensive understanding of a person's health, promoting optimal wellbeing.</p>
-
+          <p className='value-prop-summary'>
+            <del>Functional nutrition is a personalized, science-based approach that focuses on identifying and treating the root causes of health imbalances, rather than just symptoms. It considers each individual's unique genetic, biochemical, and lifestyle factors, including diet, to create a tailored health plan. It acknowledges that a single condition can have multiple causes, and one cause can lead to various conditions, depending on an individual's specific circumstances. This method provides a comprehensive understanding of a person's health, promoting optimal wellbeing.</del>
+          </p>
         </article>
       </div>
           <div className="button-container">
